4f72c3a3bf001a3d7a4049570aba9246de5a9672
tush <wafulaat@gmail.com>
2022-11-08 07:35:06 +0300

    password reset

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.HelloAuthView.as_view(), name='hello_auth'),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('signup/input/', views.InputCreateView.as_view(), name='sign_up'),
     path('signup/investor/', views.InvestorCreateView.as_view(), name='sign_up'),
     path('signout/', views.UserLogoutView.as_view(), name='sign_out'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
--- farm/settings.py
+++ farm/settings.py
@@ -173,3 +173,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'agreywafula147@gmail.com'
+EMAIL_HOST_PASSWORD = 'ydffgbtvhympyjhi'
