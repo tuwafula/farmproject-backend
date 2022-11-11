3f22819c9d121626fde700e94bdcd20111af3f93
tuwafula <84623103+tuwafula@users.noreply.github.com>
2022-11-12 01:15:27 +0300

    Update settings.py

--- farm/settings.py
+++ farm/settings.py
@@ -182,4 +182,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'agreywafula147@gmail.com'
-EMAIL_HOST_PASSWORD = 'ydffgbtvhympyjhi'
+EMAIL_HOST_PASSWORD = HOST_PASSWORD
